d0e291a9729de57c6ecb644abd9c7b688b1f6d7b
StevenJAcobs61 <stevenjacobs61@gmail.com>
2024-06-18 14:24:22 +0100

    navbar

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/navbar.scss";
 import { Link } from "monday-ui-react-core";
+import Logo from "../../img/timing-logo.png";
+import { navlinks } from "../../utils/data";
 
 export default function Navbar() {
   return (
     <div className="navbar__container">
       <div className="navbar__wrapper">
-        <div className="navbar__logo" />
+        <div className="navbar__logo">
+          <img src={Logo} alt="Logo" />
+        </div>
         <div className="navbar__links-cont">
-          <Link className="navbar__link" text="Timelogs" />
-          <Link className="navbar__link" text="Ratecards" />
-          <Link className="navbar__link" text="Settings" />
+          {navlinks.map((link) => (
+            <Link
+              key={link.href}
+              textClassName={`navbar__link ${
+                window.location.pathname === link.href ? "highlight" : ""
+              }`}
+              text={link.text}
+              href={link.href}
+              target={Link.targets.SELF}
+            />
+          ))}
         </div>
       </div>
     </div>
---
+++ src/utils/data.js
@@ -0,0 +1,18 @@
+export const navlinks = [
+  {
+    href: "/",
+    text: "Entries",
+  },
+  {
+    href: "/people",
+    text: "People",
+  },
+  {
+    href: "/export",
+    text: "Export",
+  },
+  {
+    href: "/settings",
+    text: "Settings",
+  },
+];
